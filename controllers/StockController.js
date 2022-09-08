8adcbd724a4d2d51c4483214f7c99567adabaa3d
frank-booth <jfraley519@gmail.com>
2022-09-08 11:05:05 -0400

    added additional stock route

--- controllers/StockController.js
+++ controllers/StockController.js
@@ -1,54 +1,62 @@
-const {Stock,User}=require('../models')
+const { Stock, User } = require('../models')
 
-
-const getUserStock = async(req,res) =>{
-    try{
-        let stockId=req.params.stock_id
-        const stock= await Stock.findByPk(stockId)
-        res.send(stock)
-    }
-    catch(error){
-        throw error
-    }
+const getAllStocks = async (req, res) => {
+  try {
+    const stocks = await Stock.findAll()
+    res.send(stocks)
+  } catch (error) {
+    throw error
+  }
 }
 
-const createUserStocks = async (req,res) => {
-    try{
-        let userId=parseInt(req.params.user_id)
-        let newStock={userId,...req.body}
-        let stock = await Stock.create(newStock)
-        res.send(newStock)
-    }
-    catch(error){
-        throw error
-    }
+const getUserStock = async (req, res) => {
+  try {
+    let stockId = req.params.stock_id
+    const stock = await Stock.findByPk(stockId)
+    res.send(stock)
+  } catch (error) {
+    throw error
+  }
 }
 
-const updateStock= async(req,res) =>{
-    try{
-        let stockId=parseInt(req.params.stock_id)
-        let updatedStock=await Stock.update(req.body,{where:{id:stockId}, returning:true})
-        res.send(updatedStock)
-    }
-    catch(error){
-        throw error
-    }
+const createUserStocks = async (req, res) => {
+  try {
+    let userId = parseInt(req.params.user_id)
+    let newStock = { userId, ...req.body }
+    let stock = await Stock.create(newStock)
+    res.send(newStock)
+  } catch (error) {
+    throw error
+  }
 }
 
-const deleteStock=async(req,res) =>{
-    try{
-        let stockId=parseInt(req.params.stock_id)
-        await Stock.destroy({where:{id:stockId}, returning:true})
-        res.send(`Stock with id of ${stockId} successfully deleted`)
-    }
-    catch(error){
-        throw error
-    }
+const updateStock = async (req, res) => {
+  try {
+    let stockId = parseInt(req.params.stock_id)
+    let updatedStock = await Stock.update(req.body, {
+      where: { id: stockId },
+      returning: true
+    })
+    res.send(updatedStock)
+  } catch (error) {
+    throw error
+  }
 }
 
-module.exports={
-    getUserStock,
-    createUserStocks,
-    updateStock,
-    deleteStock
+const deleteStock = async (req, res) => {
+  try {
+    let stockId = parseInt(req.params.stock_id)
+    await Stock.destroy({ where: { id: stockId }, returning: true })
+    res.send(`Stock with id of ${stockId} successfully deleted`)
+  } catch (error) {
+    throw error
+  }
+}
+
+module.exports = {
+  getAllStocks,
+  getUserStock,
+  createUserStocks,
+  updateStock,
+  deleteStock
 }
--- routes/StockRouter.js
+++ routes/StockRouter.js
@@ -1,10 +1,10 @@
 const Router = require('express').Router()
-const controller=require('../controllers/StockController')
+const controller = require('../controllers/StockController')
 
-Router.get('/:stock_id',controller.getUserStock)
-Router.post('/:user_id',controller.createUserStocks)
-Router.put('/:stock_id',controller.updateStock)
-Router.delete('/:stock_id',controller.deleteStock)
+Router.get('/', controller.getAllStocks)
+Router.get('/:stock_id', controller.getUserStock)
+Router.post('/:user_id', controller.createUserStocks)
+Router.put('/:stock_id', controller.updateStock)
+Router.delete('/:stock_id', controller.deleteStock)
 
-
-module.exports=Router
+module.exports = Router
